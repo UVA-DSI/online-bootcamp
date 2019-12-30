f5ace774d5bef631ca544a0df9caf6842ebeb84d
eldobuva <59130282+eldobuva@users.noreply.github.com>
2019-12-30 16:16:46 -0500

    Add files via upload

---
+++ Python.Time.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+# Set Date-Time Variable
+import datetime
+Time = datetime.datetime.now()
+#Print Time
+print(str(Time))
