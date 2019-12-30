71e91706899b624cc245f38403d9629cd99fba22
eldobuva <59130282+eldobuva@users.noreply.github.com>
2019-12-30 18:14:48 -0500

    Python List

---
+++ Python.List.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#1 Data Entry
+Data1 = input("What is your name? ")
+Data2 = input("Where are you from? ")
+Data3 = input("How old are you? ")
+Data4 = input("What is your favorite color? ")
+List = [Data1, Data2, Data3, Data4]
+for i in List:
+    print(i)
+
+        
+
