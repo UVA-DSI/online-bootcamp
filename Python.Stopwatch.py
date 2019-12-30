7fb7f02f44a2881d27d1a1bd2e853b43a9087e1b
eldobuva <59130282+eldobuva@users.noreply.github.com>
2019-12-30 16:38:01 -0500

    Python Stopwatch

---
+++ Python.Stopwatch.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import time
+
+#1 Enter Starting Time
+input("Press Enter to start")
+start_time = time.time()
+
+#2 Enter Ending Time
+input("Press Enter to stop")
+end_time = time.time()
+
+#3 Calculate Length of Time
+diff = end_time - start_time
+
+#4 Print
+print(str(start_time))
+print(str(end_time))
+print(str(diff))
+
+
+
+
+
+
