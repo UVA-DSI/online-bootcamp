26d583da36ffa0386301b9a243ab0ba94ec0d4a3
hs4cf <59421608+hs4cf@users.noreply.github.com>
2020-01-04 12:41:35 -0500

    Hanim's R Exercise

---
+++ Exercise3_PrintUser.R
@@ -0,0 +1,7 @@
+#Step 1: Figure out how to get input from the user using the keyboard.
+
+my.name <- readline(prompt= "Enter name: ")
+#Step 2: Record that input in a variable.
+
+#Step 3: Print that variable.
+print(my.name)
